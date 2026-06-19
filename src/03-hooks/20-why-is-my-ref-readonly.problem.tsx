a1f71395efc07aebb7ef48caa8c18bce1a53b415
Pavel Ashifin <globus40000@gmail.com>
2026-06-19 10:16:42 +0600

    feat(20): add explanation

--- src/03-hooks/20-why-is-my-ref-readonly.problem.tsx
+++ src/03-hooks/20-why-is-my-ref-readonly.problem.tsx
@@ -1,6 +1,11 @@
 import { useRef } from "react";
 
 export const Component = () => {
+  // Passing `null` tells React to manage the ref - readonly for us.
+  // const ref = useRef<string>(null);
+
+  // Padding something (except `null`) or nothing tells React that
+  // we will manage the ref by themselves.
   const ref = useRef<string>("");
 
   // Why is this not allowed?
