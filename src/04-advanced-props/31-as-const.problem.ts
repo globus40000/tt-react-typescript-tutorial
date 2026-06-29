f8a13c817d1a548b8f473f074a42ae1c7b67920e
Pavel Ashifin <globus40000@gmail.com>
2026-06-29 13:25:23 +0600

    fix(31):  use `as const`

--- src/04-advanced-props/31-as-const.problem.ts
+++ src/04-advanced-props/31-as-const.problem.ts
@@ -4,12 +4,14 @@ const BACKEND_TO_FRONTEND_STATUS_MAP = {
   0: "pending",
   1: "success",
   2: "error",
-};
+} as const;
 
-type BackendStatus = unknown;
-type FrontendStatus = unknown;
+type Map = typeof BACKEND_TO_FRONTEND_STATUS_MAP;
+
+type BackendStatus = keyof Map;
+type FrontendStatus = Map[BackendStatus];
 
 type test = [
   Expect<Equal<BackendStatus, 0 | 1 | 2>>,
-  Expect<Equal<FrontendStatus, "pending" | "success" | "error">>
+  Expect<Equal<FrontendStatus, "pending" | "success" | "error">>,
 ];
