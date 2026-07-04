2d83408cd059a13249aa60da8e7e78c920540aea
Pavel Ashifin <globus40000@gmail.com>
2026-07-04 13:19:16 +0600

    feat(36): use Record

--- src/05-generics/36-type-helpers-with-constraints.problem.tsx
+++ src/05-generics/36-type-helpers-with-constraints.problem.tsx
@@ -1,8 +1,10 @@
 import { Equal, Expect } from "../helpers/type-utils";
 
-type AllOrNothing<T extends object> = T | ToUndefinedObject<T>;
+type AllOrNothing<T extends Record<string, unknown>> = T | ToUndefinedObject<T>;
 
-type ToUndefinedObject<T extends object> = Partial<Record<keyof T, undefined>>;
+type ToUndefinedObject<T extends Record<string, unknown>> = Partial<
+  Record<keyof T, undefined>
+>;
 
 /**
  * There's a problem with our AllOrNothing type. It's letting
