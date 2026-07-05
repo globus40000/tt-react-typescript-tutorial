1d723f4a91969e4f3b1916febb895acd7bdc4a39
Pavel Ashifin <globus40000@gmail.com>
2026-07-05 16:49:35 +0600

    fix(37):  add generic type argument

--- src/05-generics/37-generic-localstorage-hook.problem.ts
+++ src/05-generics/37-generic-localstorage-hook.problem.ts
@@ -14,16 +14,16 @@ import { Equal, Expect } from "../helpers/type-utils";
  *
  * 1. Figure out a way to make this work using generics.
  */
-export const useLocalStorage = (prefix: string) => {
+export function useLocalStorage<T>(prefix: string) {
   return {
-    get: (key: string) => {
+    get: (key: string): T | null => {
       return JSON.parse(window.localStorage.getItem(prefix + key) || "null");
     },
-    set: (key: string, value: any) => {
+    set: (key: string, value: T) => {
       window.localStorage.setItem(prefix + key, JSON.stringify(value));
     },
   };
-};
+}
 
 const user = useLocalStorage<{ name: string }>("user");
 
