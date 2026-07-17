9962b58ed19abe9e869cef3bf27b944b130fc27d
Pavel Ashifin <globus40000@gmail.com>
2026-07-17 11:52:09 +0600

    fix(45): add return type

--- src/06-advanced-hooks/45-tuple-return-type.problem.ts
+++ src/06-advanced-hooks/45-tuple-return-type.problem.ts
@@ -11,7 +11,9 @@ import { Equal, Expect } from "../helpers/type-utils";
  * 1. Find a way to fix the errors below.
  */
 
-export const useId = (defaultId: string) => {
+export const useId = (
+  defaultId: string,
+): [string, React.Dispatch<React.SetStateAction<string>>] => {
   const [id, setId] = useState(defaultId);
 
   return [id, setId];
